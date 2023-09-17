bfcddd4536c4789a192e58fb7c7864905f627f46
sabireyvazli1 <sabireyvazli@gmail.com>
2023-09-17 12:32:15 +0400

    done

--- app.js
+++ app.js
@@ -1,61 +1,26 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const nameINP = document.querySelector(".nameINP");
+const surnameINP = document.querySelector(".surnameINP");
+const passwordINP = document.querySelector(".passwordINP");
+const IDINP = document.querySelector(".IDINP");
+const deleteIdINP = document.querySelector(".deleteIdINP");
+const forrm = document.querySelector(".forrm");
+const forrm2 = document.querySelector(".forrm2");
 
-app.get("/", (req, res) => res.type('html').send(html));
+forrm.addEventListener("submit", function(e){
+    e.preventDefault();
+    let obj = {};
+    obj.ad = nameINP.value;
+    obj.soyad = surnameINP.value;
+    obj.parol = passwordINP.value;
+    obj.id = IDINP.value;
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    // Use Axios to send data to your backend
+    axios.post("http://localhost:3306/student", obj);
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+forrm2.addEventListener("submit", function (e){
+    e.preventDefault();
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    const idToDelete = deleteIdINP.value;
+    axios.delete(`http://localhost:3306/student/${idToDelete}`);
+});
